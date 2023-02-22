333f187dfcdaf071a59b9b8809423e9cce52c057
諸江赳至 <moroe@housei-inc.com>
2023-02-22 14:00:18 +0900

    version2

--- Program01.java
+++ Program01.java
@@ -2,5 +2,6 @@ public class Program01 {
     public static void main {
         System.out.println('Program01-V0');
         System.out.println('Program01-V1');
+        System.out.println('Program01-V2');
     }
 }
